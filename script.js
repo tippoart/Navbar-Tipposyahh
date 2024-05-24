b3d830b34e60c1d4d870764a95ad5dc99a1f626e
tippoart <latifardiansyah982@gmail.com>
2024-05-24 20:43:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const listMenu = document.getElementById("menu");
+const menu = document.getElementById("menu-list");
+
+function toggleMenu() {
+  const menuList = document.getElementById("menu-list");
+  menuList.classList.toggle("hidden");
+}
